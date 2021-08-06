98c92e9dfe12a9a502d14c960662bae3cd11cea7
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-08-06 11:17:45 -0400

    Create model method

--- app/models/garden.rb
+++ app/models/garden.rb
@@ -1,4 +1,8 @@
 class Garden < ApplicationRecord
   has_many :plots, dependent: :destroy
   has_many :plants, through: :plots
+
+  def garden_plants
+    plants.where('days_to_harvest < ?', 100).uniq
+  end
 end
