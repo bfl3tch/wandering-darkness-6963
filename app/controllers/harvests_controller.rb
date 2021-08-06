16eca93fdbea65b45ff50769ec96b19785775e38
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-08-06 10:53:12 -0400

    User story 2 complete

--- app/controllers/harvests_controller.rb
+++ app/controllers/harvests_controller.rb
@@ -1,7 +1,6 @@
 class HarvestsController < ApplicationController
 
   def destroy
-    require "pry"; binding.pry
     Harvest.find_by(plot_id=params[plot_id], plant_id=params[plant_id]).destroy
     redirect_to plots_path
   end
