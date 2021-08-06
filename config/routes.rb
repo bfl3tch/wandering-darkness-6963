cedf6363954c10878b041ebaae839e077db5afa4
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-08-06 10:52:22 -0400

    Create route

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 resources :plots
+
+delete '/plots/:plot_id/plants/:plant_id', to: 'harvests#destroy', as: 'harvest'
+
 end
